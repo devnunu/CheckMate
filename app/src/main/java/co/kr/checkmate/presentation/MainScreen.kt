5960c5591d251b7f67a498ddd83c79f0b15966d9
eunwoo <aufcl4858@naver.com>
2025-04-22 00:42:25 +0900

    feat: 네비게이션 추가

--- app/src/main/java/co/kr/checkmate/presentation/MainScreen.kt
+++ app/src/main/java/co/kr/checkmate/presentation/MainScreen.kt
@@ -1,18 +1,37 @@
 package co.kr.checkmate.presentation
 
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import co.kr.checkmate.presentation.home.HomeScreen
-import co.kr.checkmate.presentation.home.HomeViewModel
-import org.koin.androidx.compose.koinViewModel
+import co.kr.checkmate.ui.ext.ScreenAnim
+import co.kr.checkmate.ui.ext.animComposable
+import co.kr.checkmate.ui.navigation.NavRoute
+import org.koin.androidx.compose.getViewModel
 
 @Composable
 fun MainScreen() {
-    val homeViewModel: HomeViewModel = koinViewModel()
+    val activity = LocalContext.current as ComponentActivity
+    val navController = rememberNavController()
 
-    // 홈 화면
-    HomeScreen(
-        viewModel = homeViewModel,
-    )
-
-
+    BackHandler {
+        if (!navController.popBackStack()) {
+            activity.finish()
+        }
+    }
+    NavHost(
+        navController = navController,
+        startDestination = NavRoute.Home
+    ) {
+        animComposable<NavRoute.Home>(
+            screenAnim = ScreenAnim.FADE_IN_OUT
+        ) {
+            HomeScreen(
+                viewModel = getViewModel(),
+            )
+        }
+    }
 }
