94890a19c94872d4ae0f3e0101a17506fadfb11d
eunwoo <aufcl4858@naver.com>
2025-04-28 00:02:13 +0900

    feat: 상태 표시줄 색상 변경
    - 상태 표시줄 색상을 흰색으로 변경했습니다.
    - 상태 표시줄의 아이콘이 어두운색으로 표시되도록 설정했습니다.

--- app/src/main/java/co/kr/checkmate/ui/theme/Theme.kt
+++ app/src/main/java/co/kr/checkmate/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -76,8 +77,8 @@ fun CheckMateTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            window.statusBarColor = Color.White.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
